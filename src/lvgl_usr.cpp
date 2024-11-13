c425b3253c09c6cbcfaabfa600b081ce248878e8
sbtoonz <sbtoonz@gmail.com>
2024-11-13 11:02:58 -0800

    Update UI function methods to meet new screens

--- src/lvgl_usr.cpp
+++ src/lvgl_usr.cpp
@@ -22,30 +22,13 @@ const char* booleanHelper(bool value){
 }
 
 void lvgl_set_current_leg() {
-    if (currentLoadChanged == false) return;
-    if (currentLoadBuffer != nullptr) {
-        if (strlen(currentLoadBuffer) > 0) {
-            const char *labelText = lv_label_get_text(uic_CurrentLeg);
-            if (strcmp(labelText, currentLoadBuffer) != 0) {
-                lv_label_set_text(uic_CurrentLeg, currentLoadBuffer);
-                activeLane = atoi(&labelText[3]);
-            }
-        }
-        currentLoadChanged = false;
-    } 
-    else {
-        const char *currentText = lv_label_get_text(uic_CurrentLeg);
-        if (strcmp(currentText, "None") != 0) {
-            lv_label_set_text(uic_CurrentLeg, "None");
-        }
-        currentLoadChanged = false;
-    }
+   if (currentLoadBuffer != nullptr)activeLane = atoi(&currentLoadBuffer[3]);
 }
 
 void lvgl_set_tool_status(){
-    const char *toolStatus = lv_label_get_text(uic_ToolStatus);
+    const char *toolStatus = lv_label_get_text(ui_ToolStatus);
     if (strcmp(toolStatus, currentLoadBuffer) != 0){
-        lv_label_set_text(uic_ToolStatus, booleanHelper(toolLoaded));
+        lv_label_set_text(ui_ToolStatus, booleanHelper(toolLoaded));
     }
 }
 
@@ -76,30 +59,14 @@ void lvgl_set_active_lane_color(){
 }
 
 void lvgl_set_leg_status(){
-    const char *leg1Status = lv_label_get_text(ui_Leg1Status);
-    if (strcmp(leg1Status, currentLoadBuffer) != 0){
-        lv_label_set_text(ui_Leg1Status, booleanHelper(leg1Load));
-        if(leg1Load){lv_obj_set_style_bg_color(ui_Tool0Button, loadedColor, LV_PART_MAIN);}
-        else{lv_obj_set_style_bg_color(ui_Tool0Button, unloadedColor, LV_PART_MAIN);}
-    }
-    const char *leg2Status = lv_label_get_text(ui_Leg2Status);
-    if (strcmp(leg2Status, currentLoadBuffer) != 0){
-        lv_label_set_text(ui_Leg2Status, booleanHelper(leg2Load));
-        if(leg2Load){lv_obj_set_style_bg_color(ui_Tool1Button, loadedColor, LV_PART_MAIN);}
-        else{lv_obj_set_style_bg_color(ui_Tool1Button, unloadedColor, LV_PART_MAIN);}
-    }
-    const char *leg3Status = lv_label_get_text(ui_Leg3Status);
-    if (strcmp(leg3Status, currentLoadBuffer) != 0){
-        lv_label_set_text(ui_Leg3Status, booleanHelper(leg3Load));
-        if(leg3Load){lv_obj_set_style_bg_color(ui_Tool2Button, loadedColor, LV_PART_MAIN);}
-        else{lv_obj_set_style_bg_color(ui_Tool2Button, unloadedColor, LV_PART_MAIN);}
-    }
-    const char *leg4Status = lv_label_get_text(ui_Leg4Status);
-    if (strcmp(leg4Status, currentLoadBuffer) != 0){
-        lv_label_set_text(ui_Leg4Status, booleanHelper(leg4Load));
-        if(leg4Load){lv_obj_set_style_bg_color(ui_Tool3Button, loadedColor, LV_PART_MAIN);}
-        else{lv_obj_set_style_bg_color(ui_Tool3Button, unloadedColor, LV_PART_MAIN);}
-    }
+    if(leg1Load){lv_obj_set_style_bg_color(ui_Tool0Button, loadedColor, LV_PART_MAIN);}
+    else{lv_obj_set_style_bg_color(ui_Tool0Button, unloadedColor, LV_PART_MAIN);}
+    if(leg2Load){lv_obj_set_style_bg_color(ui_Tool1Button, loadedColor, LV_PART_MAIN);}
+    else{lv_obj_set_style_bg_color(ui_Tool1Button, unloadedColor, LV_PART_MAIN);}
+    if(leg3Load){lv_obj_set_style_bg_color(ui_Tool2Button, loadedColor, LV_PART_MAIN);}
+    else{lv_obj_set_style_bg_color(ui_Tool2Button, unloadedColor, LV_PART_MAIN);}
+    if(leg4Load){lv_obj_set_style_bg_color(ui_Tool3Button, loadedColor, LV_PART_MAIN);}
+    else{lv_obj_set_style_bg_color(ui_Tool3Button, unloadedColor, LV_PART_MAIN);}
     lvgl_set_active_lane_color();
 }
 
@@ -111,13 +78,36 @@ void lvgl_set_nozzle_temp(){
     }
 }
 
+void lvgl_set_bed_temp(){
+    if(moonraker.data.nozzle_actual != atoi(lv_label_get_text(ui_CurrentBedTemp))){
+        char buffer[12];
+        sprintf(buffer, "%d", moonraker.data.bed_actual);
+        lv_label_set_text(ui_CurrentBedTemp, buffer);
+    }
+}
+
+void lvgl_toggle_current_print_panel(){
+    bool switchStatusHidden = lv_obj_has_flag(ui_SwitchStatusPanel, LV_OBJ_FLAG_HIDDEN);
+    bool printStatusHidden = lv_obj_has_flag(ui_Printstatus, LV_OBJ_FLAG_HIDDEN);
+    if(moonraker.data.printing && !switchStatusHidden && printStatusHidden){
+        if(!switchStatusHidden)
+        {
+            lv_obj_add_flag(ui_SwitchStatusPanel, LV_OBJ_FLAG_HIDDEN);
+            lv_obj_clear_flag(ui_Printstatus, LV_OBJ_FLAG_HIDDEN);
+        }
+        else{
+            lv_obj_add_flag(ui_Printstatus, LV_OBJ_FLAG_HIDDEN);
+            lv_obj_clear_flag(ui_SwitchStatusPanel, LV_OBJ_FLAG_HIDDEN);
+        }
+    }
+}
 void lvgl_set_params(){
         lvgl_set_current_leg();
         lvgl_set_tool_status();
         lvgl_set_hub_status();
         lvgl_set_leg_status();
         lvgl_set_nozzle_temp();
-        
+        lvgl_set_bed_temp();
 }
 
 void lvgl_ui_task(void * parameter) {
