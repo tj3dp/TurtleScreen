efcdd280255c1442518646d1e4b8c1847cbfd11a
sbtoonz <sbtoonz@gmail.com>
2024-11-13 20:28:48 -0800

    Revert "change endpoint and scrape method to work with my moonraker plugin that enriches with spoolman"
    This reverts commit 4d79381d739e2bd80bb0df9faba78a5fc0d8a96b.

--- src/api_fetch.cpp
+++ src/api_fetch.cpp
@@ -1,4 +1,5 @@
 #include "api_fetch.h"
+
 String apiURL;
 float eventTime;
 bool leg1Load;
@@ -65,7 +66,7 @@ void ParseAPIResponse(const String &jsonResponse)
 #ifdef DEBUGOUTPUT
     DEBUG_PRINTLN("Running API Parse");
 #endif
-    DynamicJsonDocument doc(2048); // Increase size for the new structure
+    DynamicJsonDocument doc(1024);
     DeserializationError error = deserializeJson(doc, jsonResponse);
 
     if (error)
@@ -76,151 +77,123 @@ void ParseAPIResponse(const String &jsonResponse)
     }
 
     JsonObject result = doc["result"];
-    String status = result["status"].as<String>();
+    eventTime = result["eventtime"];
+    JsonObject status = result["status"];
+    JsonObject afc = status["AFC"];
 
-    if (status != "success")
+#ifdef DEBUGOUTPUT
+    DEBUG_PRINT("Turtle_1 Object: ");
+    DEBUG_PRINTLN(afc["Turtle_1"].as<String>());
+#endif
+    for (int leg = 1; leg <= 4; ++leg)
     {
-        DEBUG_PRINTLN("API status is not success");
-        return;
+        String legKey = "leg" + String(leg);
+        if (afc.containsKey("Turtle_1") && afc["Turtle_1"].containsKey(legKey.c_str()))
+        {
+            JsonObject legData = afc["Turtle_1"][legKey];
+            bool load = legData["load"];
+            bool prep = legData["prep"];
+            loadedToHub = legData["loaded_to_hub"];
+            int lane = legData["LANE"];
+#ifdef DEBUGOUTPUT
+            DEBUG_PRINT("Lane ");
+            DEBUG_PRINT(lane);
+            DEBUG_PRINT(" - Load: ");
+            DEBUG_PRINT(load ? "true" : "false");
+            DEBUG_PRINT(", Prep: ");
+            DEBUG_PRINT(prep ? "true" : "false");
+            DEBUG_PRINT(", Loaded to Hub: ");
+            DEBUG_PRINTLN(loadedToHub ? "true" : "false");
+#endif
+            if (lane == 1)
+            {
+                leg1Load = load;
+            }
+            else if (lane == 2)
+            {
+                leg2Load = load;
+            }
+            else if (lane == 3)
+            {
+                leg3Load = load;
+            }
+            else if (lane == 4)
+            {
+                leg4Load = load;
+            }
+        }
+        else
+        {
+            DEBUG_PRINT("Checking key: ");
+            DEBUG_PRINTLN(legKey);
+            DEBUG_PRINTLN("Key not found.");
+        }
     }
 
-    JsonObject spools = result["spools"];
-    JsonObject turtle1 = spools["Turtle_1"];
-
-    if (!turtle1.isNull())
+    if (status.containsKey("AFC"))
     {
-#ifdef DEBUGOUTPUT
-        DEBUG_PRINT("Turtle_1 Object: ");
-        serializeJson(turtle1, Serial);
-        DEBUG_PRINTLN();
-#endif
-        // Iterate through the legs (leg1, leg2, leg3, leg4)
-        for (int leg = 1; leg <= 4; ++leg)
+        JsonObject afcStatus = status["AFC"];
+
+        if (afcStatus.containsKey("system"))
         {
-            String legKey = "leg" + String(leg);
-            if (turtle1.containsKey(legKey))
-            {
-                JsonObject legData = turtle1[legKey];
-                bool load = legData["load"];
-                bool prep = legData["prep"];
-                loadedToHub = legData["loaded_to_hub"];
-                String material = legData["material"].as<String>();
-                String spool_id = legData["spool_id"].as<String>();
-                String color = legData["color"].as<String>();
-                float remaining_weight = legData["remaining_weight"].as<float>();
-
-                int lane = legData["LANE"];
+            JsonObject system = afcStatus["system"];
 
 #ifdef DEBUGOUTPUT
-                DEBUG_PRINT("Lane ");
-                DEBUG_PRINT(lane);
-                DEBUG_PRINT(" - Load: ");
-                DEBUG_PRINT(load ? "true" : "false");
-                DEBUG_PRINT(", Prep: ");
-                DEBUG_PRINT(prep ? "true" : "false");
-                DEBUG_PRINT(", Loaded to Hub: ");
-                DEBUG_PRINT(loadedToHub ? "true" : "false");
-                DEBUG_PRINT(", Material: ");
-                DEBUG_PRINT(material);
-                DEBUG_PRINT(", Spool ID: ");
-                DEBUG_PRINT(spool_id);
-                DEBUG_PRINT(", Color: ");
-                DEBUG_PRINT(color);
-                DEBUG_PRINT(", Remaining Weight: ");
-                DEBUG_PRINTLN(remaining_weight);
+            DEBUG_PRINT("System Object: ");
+            serializeJson(system, Serial);
+            DEBUG_PRINTLN();
 #endif
-                // Update leg load statuses
-                if (lane == 1)
+            currentLoadChanged = false;
+            currentLoad = system["current_load"].as<const char *>();
+
+            if (currentLoad == nullptr)
+            {
+                if (currentLoadBuffer[0] != '\0')
                 {
-                    leg1Load = load;
-                }
-                else if (lane == 2)
-                {
-                    leg2Load = load;
-                }
-                else if (lane == 3)
-                {
-                    leg3Load = load;
-                }
-                else if (lane == 4)
-                {
-                    leg4Load = load;
+                    currentLoadBuffer[0] = '\0';
+                    currentLoadChanged = true;
                 }
             }
             else
             {
-#ifdef DEBUGOUTPUT
-                DEBUG_PRINT("Checking key: ");
-                DEBUG_PRINTLN(legKey);
-                DEBUG_PRINTLN("Key not found.");
-#endif
+                if (strcmp(currentLoadBuffer, currentLoad) != 0)
+                {
+                    strncpy(currentLoadBuffer, currentLoad, sizeof(currentLoadBuffer) - 1);
+                    currentLoadBuffer[sizeof(currentLoadBuffer) - 1] = '\0';
+                    currentLoadChanged = true;
+                    //
+                }
             }
+
+            toolLoaded = system["tool_loaded"];
+            loadedToHub = system["hub_loaded"];
+
+#ifdef DEBUGOUTPUT
+            DEBUG_PRINT("Raw tool_loaded value: ");
+            DEBUG_PRINTLN(toolLoaded ? "true" : "false");
+#endif
+            JsonVariant buffer = system["buffer"];
+#ifdef DEBUGOUTPUT
+            DEBUG_PRINT("Buffer: ");
+            if (buffer.isNull())
+            {
+                DEBUG_PRINTLN("null");
+            }
+            else
+            {
+                DEBUG_PRINTLN(buffer.as<String>());
+            }
+#endif
+        }
+        else
+        {
+            DEBUG_PRINTLN("System key not found in AFC.");
         }
     }
     else
     {
-#ifdef DEBUGOUTPUT
-        DEBUG_PRINTLN("Turtle_1 key not found in spools.");
-#endif
+        DEBUG_PRINTLN("AFC key not found in status.");
     }
-
-    // Parsing system information
-    JsonObject system = spools["system"];
-    if (!system.isNull())
-    {
-#ifdef DEBUGOUTPUT
-        DEBUG_PRINT("System Object: ");
-        serializeJson(system, Serial);
-        DEBUG_PRINTLN();
-#endif
-        currentLoadChanged = false;
-        currentLoad = system["current_load"].as<const char *>();
-
-        if (currentLoad == nullptr)
-        {
-            if (currentLoadBuffer[0] != '\0')
-            {
-                currentLoadBuffer[0] = '\0';
-                currentLoadChanged = true;
-            }
-        }
-        else
-        {
-            if (strcmp(currentLoadBuffer, currentLoad) != 0)
-            {
-                strncpy(currentLoadBuffer, currentLoad, sizeof(currentLoadBuffer) - 1);
-                currentLoadBuffer[sizeof(currentLoadBuffer) - 1] = '\0';
-                currentLoadChanged = true;
-            }
-        }
-
-        toolLoaded = system["tool_loaded"];
-        loadedToHub = system["hub_loaded"];
-
-#ifdef DEBUGOUTPUT
-        DEBUG_PRINT("Raw tool_loaded value: ");
-        DEBUG_PRINTLN(toolLoaded ? "true" : "false");
-#endif
-        JsonVariant buffer = system["buffer"];
-#ifdef DEBUGOUTPUT
-        DEBUG_PRINT("Buffer: ");
-        if (buffer.isNull())
-        {
-            DEBUG_PRINTLN("null");
-        }
-        else
-        {
-            DEBUG_PRINTLN(buffer.as<String>());
-        }
-#endif
-    }
-    else
-    {
-#ifdef DEBUGOUTPUT
-        DEBUG_PRINTLN("System key not found in spools.");
-#endif
-    }
-
 #ifdef DEBUGOUTPUT
     DEBUG_PRINT("Lane 1 Status: ");
     DEBUG_PRINTLN(leg1Load);
