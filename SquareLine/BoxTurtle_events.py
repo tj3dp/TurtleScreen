f367a71d2cfa0c7f3d943cc5957a01b7b778650e
sbtoonz <sbtoonz@gmail.com>
2025-02-12 19:40:35 -0800

    update squareline project
    Signed-off-by: sbtoonz <sbtoonz@gmail.com>

---
+++ SquareLine/BoxTurtle_events.py
@@ -0,0 +1,44 @@
+
+def TestFunc(event_struct):
+    return
+
+
+def saveColorWheel(event_struct):
+    return
+
+
+def EjectLane(event_struct):
+    return
+
+
+def SetLaneActive(event_struct):
+    return
+
+
+def afcBrushCall(event_struct):
+    return
+
+
+def afcCutCall(event_struct):
+    return
+
+
+def afcKickCall(event_struct):
+    return
+
+
+def afcParkCall(event_struct):
+    return
+
+
+def afcPoopCall(event_struct):
+    return
+
+
+def btPrepCall(event_struct):
+    return
+
+
+def toolUnloadCall(event_struct):
+    return
+
